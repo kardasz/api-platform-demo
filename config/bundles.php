9751a0f68cdf7f03d4289c85905a9d9eac50b77b
Krzysztof Kardasz <krzysztof@kardasz.eu>
2021-05-11 20:33:54 +0200

    ApiPlatform Demo

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,7 @@ return [
     Trikoder\Bundle\OAuth2Bundle\TrikoderOAuth2Bundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
